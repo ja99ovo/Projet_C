63f6d7e8ebb0b90f59d4c210e82d9c4e12a0ba8b
ja99ovo <ja99ovo@gmail.com>
2022-11-19 20:32:43 +0100

    Create main.c
    plan

---
+++ main.c
@@ -0,0 +1,12 @@
+/*interface
+
+choisir le fonctionnement
+  1. compresser
+      function 1 : read file (char* file_name) return char* file_content
+      function 2 : compter fréquence (char* file_content) return struct fréq(char caractère, int fois)
+      function 3 : générer l'arbre (struct fréq ) return struct Harbre arbre
+      function 4 : générer le tableau de codage (Harbre arbre) return tableau
+      function 5 : coder la texte et sauvgarder en txt (char* file_content, Harbre arbre) return void
+  2. décompresser
+
+*/
