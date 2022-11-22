a352518602c6324dc3ba21a9ba56d9ac9883b9b5
ja99ovo <ja99ovo@gmail.com>
2022-11-22 08:40:57 +0100

    22/11

--- main.c
+++ main.c
@@ -11,18 +11,30 @@ choisir le fonctionnement
       indéterminé
 */
 
+#include <math.h>
+typedef struct Htree{
+  char ch[1];
+  int weight;
+  struct Htree *parent_node;
+  struct Htree *child_node_0;
+  struct Htree *child_node_1;
+} Htree,Hnode;
 
-struct Htree{
+struct Htable{
+  char ch;
+  long code;
 
 };
-struct Freq{
 
 
-};
 
 int main(){
   
+Hnode ht1={"a",5,&ht1,&ht1,&ht1};
+Hnode ht2={"b",3,&ht1,&ht1,&ht1};
+Hnode ht3={"c",1,&ht1,&ht1,&ht1};
 
+Hnode hts[3]={ht1,ht2,ht3};
 
 }
 
@@ -33,11 +45,48 @@ char* Read_file(char* file_name){
   char* file_content;
   //
   return (file_content);
-}
+};
 
- struct Htree Compt_freq(char* file_content){
+//Hnode[] Compt_freq(char* file_content){
+
+
+
+//}
+
+
+
+
+
+Htree Create_tree(Htree Hnode[],int length){
+
+  Htree *HT = (Htree*)malloc(sizeof(Htree)*(length+1));
+  int min0 = INFINITY;
+  int min1 = INFINITY;
+  Hnode Hnode_min_1,Hnode_min_0;
+  for(int i=0;i<length;i++)
+  {
+    if (Hnode[i]->weight < min1){
+      Hnode_min_0 = Hnode[i];
+      Hnode[i]->height=INFINITY;
+    }
+  }
+
+  for(int i=0;i<length;i++){
+    if (Hnode[i]->weight < min0){
+      Hnode_min_1 = Hnode[i];
+      Hnode[i]->height=INFINITY;
+    }
+  }
+    
   
+
 }
 
+struct Htable Create_table(){
 
 
+}
+
+void Codage(){
+
+}
